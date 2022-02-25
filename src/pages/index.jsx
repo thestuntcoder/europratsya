5e3b93e71d6a438d5ea8f09d30632a86a22b4811
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-25 21:41:12 +0100

    Add navigation to the homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import LayoutPage from '../components/layout/layout-page';
+import NavCenter from '../components/layout/nav-center';
 
 export default function Home() {
   return (
@@ -8,6 +9,14 @@ export default function Home() {
       <Helmet>
         <title>Europratsya</title>
       </Helmet>
+
+      <div className="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+        <NavCenter />
+      </div>
+
+      <div className="relative bg-white">
+        <div className="lg:absolute lg:inset-0" />
+      </div>
     </LayoutPage>
   );
 }
