6bc508eba0523f73b3aabe88009dc62e4fae459f
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-27 23:59:11 +0100

    Add search to header

--- src/components/layout/nav-center.jsx
+++ src/components/layout/nav-center.jsx
@@ -69,14 +69,35 @@ export default class NavCenter extends React.Component {
               </div>
             </div>
             <div className="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
-              <span className="inline-flex rounded-md shadow">
-                <Link
-                  className="focus:shadow-outline-blue inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium leading-6 text-blue-600 transition duration-150 ease-in-out hover:text-blue-500 focus:border-blue-300 focus:outline-none active:bg-gray-50 active:text-blue-700"
-                  to="/contact/"
-                >
-                  Contact us
-                </Link>
-              </span>
+              <div class="max-w-lg w-full lg:max-w-xs">
+                <label for="search" class="sr-only">
+                  Search
+                </label>
+                <div class="relative">
+                  <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <svg
+                      class="h-5 w-5 text-gray-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    id="search"
+                    name="search"
+                    class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-gray-100 text-gray-600 placeholder-gray-400 focus:outline-none focus:bg-gray-200 focus:border-blue-500 focus:ring-grey-300 focus:text-gray-900 sm:text-sm"
+                    placeholder="Search"
+                    type="search"
+                  />
+                </div>
+              </div>
             </div>
           </nav>
         </div>
