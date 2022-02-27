c1eccb0e47ca21aaf736444f3d00f9decabb76da
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-27 23:05:12 +0100

    Add orgnisation informatinon to the footer

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -7,7 +7,8 @@ export default function Footer() {
       <div className="mx-auto max-w-screen-xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div className="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
           <p className="mt-8 text-base leading-6 text-gray-400 md:order-1 md:mt-0">
-            © 2022 — All rights reserved. /{' '}
+            © 2022 — All rights reserved. / Europratsya CIC, 32 Oakbank
+            Crescent, UK - Perth PH1 1DF /{' '}
             <a href="mailto:info@europratsya.com">info@europratsya.com</a>
           </p>
         </div>
