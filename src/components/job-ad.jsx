95645d350c090538d3c263ca3b4fde94ed3786a7
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-27 23:41:18 +0100

    Make sure Job ad shows more meaningful information

--- src/components/job-ad.jsx
+++ src/components/job-ad.jsx
@@ -2,52 +2,36 @@ import * as React from 'react';
 
 export default function JobAd() {
   return (
-    <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-      <div class="flex-shrink-0">
-        <img
-          class="h-48 w-full object-cover"
-          src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-          alt=""
-        />
-      </div>
-      <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-        <div class="flex-1">
-          <p class="text-sm font-medium text-blue-500">
-            <a href="#" class="hover:underline">
-              {' '}
-              Stuttgart, Germany{' '}
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+      <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1">
+          <p className="text-sm font-medium text-blue-500">
+            <a href="#" className="hover:underline">
+              Stuttgart, Germany
             </a>
           </p>
-          <a href="#" class="block mt-2">
-            <p class="text-xl font-semibold text-gray-900">Data Analyst</p>
-            <p class="mt-3 text-base text-gray-500">
+          <a href="#" className="block mt-2">
+            <p className="text-xl font-semibold text-gray-900">Data Analyst</p>
+            <p className="mt-3 text-base text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto accusantium praesentium eius, ut atque fuga culpa,
               similique sequi cum eos quis dolorum.
             </p>
           </a>
         </div>
-        <div class="mt-6 flex items-center">
-          <div class="flex-shrink-0">
-            <a href="#">
-              <span class="sr-only">Addidas</span>
-              <img
-                class="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </a>
-          </div>
-          <div class="ml-3">
-            <p class="text-sm font-medium text-gray-900">
-              <a href="#" class="hover:underline">
-                {' '}
-                Addidas{' '}
+        <div className="mt-6 flex items-center">
+          <div>
+            <p className="text-sm font-medium text-gray-900">
+              <a href="#" className="hover:underline">
+                Addidas
               </a>
             </p>
-            <div class="flex space-x-1 text-sm text-gray-500">
-              <time datetime="2020-03-16"> Mar 16, 2022 </time>
-              <span aria-hidden="true"> &middot; </span>
+            <div className="flex space-x-1 text-sm text-gray-500">
+              Visa requirement
+              <br />
+              Salary
+              <br />
+              Contact detail
             </div>
           </div>
         </div>
