ca52c43d49dd1b5f6af959c440c0cc1bbc2127d1
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-27 23:29:59 +0100

    Replace wrong "class" with "className" in the homepage

--- src/pages/index.en.jsx
+++ src/pages/index.en.jsx
@@ -21,32 +21,32 @@ export default function Home() {
         <div className="lg:absolute lg:inset-0" />
       </div>
 
-      <div class="relative bg-white">
-        <div class="lg:absolute lg:inset-0">
-          <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 overflow-hidden">
+      <div className="relative bg-white">
+        <div className="lg:absolute lg:inset-0">
+          <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 overflow-hidden">
             <img
-              class="h-56 w-full object-cover lg:absolute lg:h-full"
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
               src={schengenImg}
               alt="Schengen area"
             />
           </div>
         </div>
-        <div class="relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2">
-          <div class="lg:pr-8">
-            <div class="max-w-md mx-auto sm:max-w-lg lg:mx-0">
-              <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
+        <div className="relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2">
+          <div className="lg:pr-8">
+            <div className="max-w-md mx-auto sm:max-w-lg lg:mx-0">
+              <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
                 Ukrainians are warmly welcomed everywhere in Europe
               </h2>
-              <p class="mt-4 text-lg text-gray-500 sm:mt-3">
+              <p className="mt-4 text-lg text-gray-500 sm:mt-3">
                 Check in here for job opportunities and introductions to
                 European employers who actively seek Ukrainian colleagues - no
                 work visas required inside Schengen.
               </p>
-              <p class="mt-4 text-lg text-gray-500 sm:mt-3">
+              <p className="mt-4 text-lg text-gray-500 sm:mt-3">
                 Look out for our guide to securing work visas in the UK,
                 Ireland, Bulgaria and Croatia.
               </p>
-              <p class="mt-4 text-lg text-gray-500 sm:mt-3">
+              <p className="mt-4 text-lg text-gray-500 sm:mt-3">
                 To offer jobs to Ukrainians please email{' '}
                 <a href="mailto:info@europratsya.com">info@europratsya.com</a>
               </p>
@@ -55,24 +55,24 @@ export default function Home() {
         </div>
       </div>
 
-      <div class="relative bg-gray-800">
-        <div class="h-56 bg-blue-500 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-          <div class="ml-8 pt-16">
-            <p class="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
+      <div className="relative bg-gray-800">
+        <div className="h-56 bg-blue-500 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+          <div className="ml-8 pt-16">
+            <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
               Are you an Employer
             </p>
-            <p class="mt-3 text-lg text-gray-300">
+            <p className="mt-3 text-lg text-gray-300">
               register to post your vacancies
             </p>
-            <div class="mt-8">
-              <div class="inline-flex rounded-md shadow">
+            <div className="mt-8">
+              <div className="inline-flex rounded-md shadow">
                 <a
                   href="#"
-                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
                 >
                   Join
                   <svg
-                    class="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                    className="-mr-1 ml-3 h-5 w-5 text-gray-400"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -86,23 +86,23 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-          <div class="md:ml-auto md:w-1/2 md:pl-10">
-            <p class="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
+        <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+          <div className="md:ml-auto md:w-1/2 md:pl-10">
+            <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
               Are you a Candidate
             </p>
-            <p class="mt-3 text-lg text-gray-300">
+            <p className="mt-3 text-lg text-gray-300">
               and need CV translation help?
             </p>
-            <div class="mt-8">
-              <div class="inline-flex rounded-md shadow">
+            <div className="mt-8">
+              <div className="inline-flex rounded-md shadow">
                 <a
                   href="#"
-                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
+                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
                 >
                   Visit the help center
                   <svg
-                    class="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                    className="-mr-1 ml-3 h-5 w-5 text-gray-400"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -118,16 +118,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div class="absolute inset-0">
-          <div class="bg-white h-1/3 sm:h-2/3" />
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3" />
         </div>
-        <div class="relative max-w-7xl mx-auto">
-          <div class="text-center">
-            <h2 class="text-3xl tracking-tight font-extrabold text-yellow-400 sm:text-4xl">
+        <div className="relative max-w-7xl mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl tracking-tight font-extrabold text-yellow-400 sm:text-4xl">
               Latest ads
             </h2>
-            <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
               Find employers which meet EU work regulations, so there is no
               exploitation for migrant workers.
             </p>
@@ -136,15 +136,15 @@ export default function Home() {
         </div>
       </div>
 
-      <div class="bg-gray-100">
-        <div class="max-w-7xl mx-auto lg:flex lg:items-center lg:justify-between">
+      <div className="bg-gray-100">
+        <div className="max-w-7xl mx-auto lg:flex lg:items-center lg:justify-between">
           <img
-            class="w-full lg:h-full lg:w-auto lg:max-w-none"
+            className="w-full lg:h-full lg:w-auto lg:max-w-none"
             src={ukeduImg}
             alt="Ukrainian education"
           />
         </div>
-        <div class="max-w-7xl mx-auto lg:flex lg:items-center lg:justify-between">
+        <div className="max-w-7xl mx-auto lg:flex lg:items-center lg:justify-between">
           <p className="text-2xl text-gray-500 text-center max-w-3xl mx-auto mt-8 mb-8">
             Several Nobel prize winners in medicine, physics, chemistry and
             literature hail from Ukraine. Its oldest universities are from the
@@ -154,50 +154,50 @@ export default function Home() {
         </div>
       </div>
 
-      <div class="bg-white">
-        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-          <p class="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <p className="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
             Sponsors and partners
           </p>
-          <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
                 alt="Workcation"
               />
             </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
                 alt="Mirage"
               />
             </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
                 alt="Tuple"
               />
             </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg"
                 alt="Laravel"
               />
             </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
                 alt="StaticKit"
               />
             </div>
-            <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
               <img
-                class="max-h-12"
+                className="max-h-12"
                 src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg"
                 alt="Statamic"
               />
