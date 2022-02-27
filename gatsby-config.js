3b11010e2777703a60891a091ce74ed299b65f94
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-28 00:10:22 +0100

    Add loading .env and Algolia settings to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: `Europratsya`,
@@ -41,6 +43,14 @@ module.exports = {
         path: './src/pages/',
       },
       __key: 'pages',
+    },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require('./src/utils/algolia-queries'),
+      },
     },
   ],
 };
