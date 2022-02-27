f9437af7b21ac055833f9f0460c24e0da6c5a88a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-28 00:15:13 +0100

    Add gatsby-plugin-styled-components to Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-plugin-i18n',
       options: {
