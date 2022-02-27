b9e06fda2a225f74f0886d6aa03c8e4558124982
Dejan Jacimovic <dejan@stuntcoders.com>
2022-02-27 23:48:52 +0100

    Render passed props to a job ad

--- src/components/job-ad.jsx
+++ src/components/job-ad.jsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 
-export default function JobAd() {
+export default function JobAd(props) {
   return (
     <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
           <p className="text-sm font-medium text-blue-500">
             <a href="#" className="hover:underline">
-              Stuttgart, Germany
+              {props.location}
             </a>
           </p>
           <a href="#" className="block mt-2">
-            <p className="text-xl font-semibold text-gray-900">Data Analyst</p>
+            <p className="text-xl font-semibold text-gray-900">
+              {props.position}
+            </p>
             <p className="mt-3 text-base text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto accusantium praesentium eius, ut atque fuga culpa,
@@ -23,15 +25,15 @@ export default function JobAd() {
           <div>
             <p className="text-sm font-medium text-gray-900">
               <a href="#" className="hover:underline">
-                Addidas
+                {props.employer}
               </a>
             </p>
             <div className="flex space-x-1 text-sm text-gray-500">
-              Visa requirement
+              {props.visa_requirement}
               <br />
-              Salary
+              {props.salary}
               <br />
-              Contact detail
+              {props.contact_detail}
             </div>
           </div>
         </div>
