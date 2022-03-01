96778f0eed113a28193bd3d69bb48fb973f80530
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-01 12:52:29 +0100

    Remove Algolia temporarily

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,13 +45,5 @@ module.exports = {
       },
       __key: 'pages',
     },
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        queries: require('./src/utils/algolia-queries'),
-      },
-    },
   ],
 };
