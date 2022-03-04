4199d694c25b629c3562c71f330d6f2f0e802751
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-04 12:52:09 +0100

    Add about us page content

--- src/pages/about-us.en.jsx
+++ src/pages/about-us.en.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
+import polarImg from '../images/about/vernadsky-polar-research-station.jpg';
 
 export default function AboutUs() {
   return (
@@ -14,45 +15,118 @@ export default function AboutUs() {
         <NavCenter />
       </div>
 
-      <div class="relative overflow-hidden bg-white py-16">
-        <div class="relative px-4 sm:px-6 lg:px-8">
-          <div class="mx-auto mb-6 max-w-prose text-lg">
-            <p class="text-center text-base font-semibold uppercase leading-6 tracking-wide text-blue-500">
-              Learn more
-            </p>
-            <h1 class="mt-2 mb-8 text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-              About Us
-            </h1>
-            <p class="text-xl leading-8 text-gray-500">
-              Europratsy.com was developed by a team of digital workers from
-              Poland, Scandinavia, Serbia and Germany.
-            </p>
+      <div class="bg-white overflow-hidden">
+        <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
+          <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+            <div>
+              <h2 class="text-base text-black font-bold text-3xl tracking-wide">
+                About us
+              </h2>
+            </div>
           </div>
-          <div class="prose prose-lg mx-auto text-gray-500">
-            <p>
-              Our small group wanted to ensure that skills and expertise aren’t
-              wasted when Ukrainian families are building new homes, whether
-              temporary or for the future.
-            </p>
-            <p>
-              We want to match the needs of European employers with the
-              qualifications of the highly educated Ukrainian people.
-            </p>
-            <p>
-              We also want to simplify research and job-hunting for people who
-              are on the move, most of them mothers who have responsibility for
-              children.
-            </p>
-            <p>
-              Europratsy CIC was founded in the UK as a not-for-profit
-              organisation to fund the web-hosting and to employ the Ukrainian
-              employees who deliver the translation services. Volunteers across
-              Europe research and post updates on employment law and visas.
-            </p>
-            <p>
-              If you have expertise to support us, please email
-              info@europratsy.com
-            </p>
+          <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div class="relative lg:row-start-1 lg:col-start-2">
+              <svg
+                class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
+                width="404"
+                height="384"
+                fill="none"
+                viewBox="0 0 404 384"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                    x="0"
+                    y="0"
+                    width="20"
+                    height="20"
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x="0"
+                      y="0"
+                      width="4"
+                      height="4"
+                      class="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width="404"
+                  height="384"
+                  fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
+                />
+              </svg>
+              <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+                <figure>
+                  <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <img
+                      class="rounded-lg shadow-lg object-cover object-center"
+                      src={polarImg}
+                      alt="Vernadsky polar research station"
+                      width="1184"
+                      height="1376"
+                    />
+                  </div>
+                  <figcaption class="mt-3 flex text-sm text-gray-500">
+                    <svg
+                      class="flex-none w-5 h-5 text-gray-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <span class="ml-2">
+                      Vernadsky polar research station. 12 Ukrainian scientists
+                      winter here for 10 months in extreme conditions,
+                      delivering widely acclaimed climate change research.{' '}
+                    </span>
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="mt-8 lg:mt-0">
+              <div class="prose prose-indigo text-gray-500 text-lg mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+                <p>
+                  Europratsya.com was developed by a team of digital workers
+                  from Serbia, Scotland and Italy.
+                </p>
+                <p>
+                  Our small group wanted to ensure that skills and expertise
+                  aren’t wasted when Ukrainian families are building new homes,
+                  whether temporary or for the future.{' '}
+                </p>
+                <p>
+                  We want to match the needs of European employers with the
+                  qualifications of the highly educated Ukrainian people.
+                </p>
+                <p>
+                  We also want to simplify research and job-hunting for people
+                  who are on the move.
+                </p>
+                <p>
+                  Europratsya CIC was founded in the UK as a not-for-profit
+                  organisation to fund the web-hosting and to employ the
+                  Ukrainian employees who deliver the translation services.
+                  Volunteers across Europe research and post updates on
+                  employment law and visas.{' '}
+                </p>
+                <p>
+                  If you have expertise to support us, please email{` `}
+                  <a href="mailto:info@europratsya.com">info@europratsya.com</a>
+                  .
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
