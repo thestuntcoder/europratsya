1c6ddb724578d66f13f9107153510a3fdd724000
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-07 01:03:38 +0100

    Add country by slug template

---
+++ src/templates/country.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
+import LayoutPage from '../components/layout/layout-page';
+import NavCenter from '../components/layout/nav-center';
+
+export const query = graphql`
+  query CountryTemplateQuery($id: String!) {
+    country: sanityCountry(id: { eq: $id }) {
+      title
+    }
+  }
+`;
+
+const Country = (props) => {
+  const { data = {}, errors } = props;
+  const { title } = data.country || {};
+
+  return (
+    <LayoutPage>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <div className="relative">
+        <NavCenter />
+      </div>
+
+      <div class="bg-white overflow-hidden">
+        <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <h1>{title}</h1>
+        </div>
+      </div>
+    </LayoutPage>
+  );
+};
+
+export default Country;
