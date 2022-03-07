8ee11ade55417ff5d457108792eb8f37ab1c9777
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-07 18:16:25 +0100

    Complete contact form for employers according to design

--- src/pages/contact-for-employers.jsx
+++ src/pages/contact-for-employers.jsx
@@ -2,128 +2,207 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
+import contactImg from '../images/contact/contact-form-employers.jpg';
 
 export default function ContactEmployers() {
   return (
     <LayoutPage>
       <Helmet>
-        <title>Contact for Employers | Europratsya</title>
+        <title>Contact for employers of Ukrainians | Europratsya</title>
       </Helmet>
 
       <div className="relative">
         <NavCenter />
       </div>
 
-      <div class="relative bg-white">
-        <div class="absolute inset-0">
-          <div class="absolute inset-y-0 left-0 w-1/2 bg-gray-50"></div>
-        </div>
-        <div class="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
-          <div class="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-            <div class="max-w-lg mx-auto">
-              <h2 class="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-                Get in touch
+      <div className="overflow-hidden bg-white">
+        <div className="max-w-7xl sm:px-6 lg:px-8 relative px-4 py-16 mx-auto">
+          <div className="lg:block bg-gray-50 left-3/4 absolute top-0 bottom-0 hidden w-screen"></div>
+          <div className="max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none mx-auto text-base">
+            <div>
+              <h2 className="text-base text-3xl font-bold tracking-wide text-black">
+                Helping you reach Ukrainians
               </h2>
-              <p class="mt-3 text-lg leading-6 text-gray-500">
-                We prefer contact form or email, but do understand sometimes
-                it's easier to reach out in a different way.
-              </p>
-              <dl class="mt-8 text-base text-gray-500">
-                <div>
-                  <dt class="sr-only">Postal address</dt>
-                  <dd>
-                    <p>Europratsya CIC</p>
-                    <p>32 Oakbank Crescent</p>
-                    <p>Perth PH1 1DF, UK</p>
-                  </dd>
-                </div>
-                <div class="mt-6">
-                  <dt class="sr-only">Email</dt>
-                  <dd class="flex">
+            </div>
+          </div>
+          <div className="lg:grid lg:grid-cols-2 lg:gap-8 mt-8">
+            <div className="lg:row-start-1 lg:col-start-2 relative">
+              <div className="max-w-prose lg:max-w-none relative mx-auto text-base">
+                <figure>
+                  <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <img
+                      className="object-cover object-center rounded-lg shadow-lg"
+                      src={contactImg}
+                      alt="Košice in Slovakia"
+                      width="1184"
+                      height="1376"
+                    />
+                  </div>
+                  <figcaption className="flex mt-3 text-sm text-gray-500">
                     <svg
-                      class="flex-shrink-0 h-6 w-6 text-gray-400"
+                      className="flex-none w-5 h-5 text-gray-400"
                       xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
                       aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                        fill-rule="evenodd"
+                        d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                        clip-rule="evenodd"
                       />
                     </svg>
-                    <span class="ml-3"> info@europratsya.com </span>
-                  </dd>
-                </div>
-              </dl>
+                    <span className="ml-2">
+                      Central business district of Košice in Slovakia, which
+                      offers a mix of tech, manufacturing and logistics careers.
+                    </span>
+                  </figcaption>
+                </figure>
+              </div>
             </div>
-          </div>
-          <div class="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
-            <div class="max-w-lg mx-auto lg:max-w-none">
-              <form action="#" method="POST" class="grid grid-cols-1 gap-y-6">
-                <div>
-                  <label for="full-name" class="sr-only">
-                    Full name
-                  </label>
-                  <input
-                    type="text"
-                    name="full-name"
-                    id="full-name"
-                    autocomplete="name"
-                    class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    placeholder="Full name"
-                  />
-                </div>
-                <div>
-                  <label for="email" class="sr-only">
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autocomplete="email"
-                    class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    placeholder="Email"
-                  />
-                </div>
-                <div>
-                  <label for="phone" class="sr-only">
-                    Phone
-                  </label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    autocomplete="tel"
-                    class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
-                    placeholder="Phone"
-                  />
-                </div>
-                <div>
-                  <label for="message" class="sr-only">
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    rows="4"
-                    class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md"
-                    placeholder="Message"
-                  ></textarea>
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    class="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+            <div className="lg:mt-0 mt-8">
+              <div className="prose-blue lg:max-w-none lg:row-start-1 lg:col-start-1 mx-auto text-lg prose text-gray-500">
+                <p>
+                  If your company wants to interview Ukrainian candidates, we
+                  make it free and simple for you with expert translations of
+                  your vacancy advertisement into Ukrainian.
+                </p>
+                <p>
+                  You will help candidates search a Europe-wide database of
+                  relevant jobs in their own language.
+                </p>
+                <p>
+                  Fill in our contact form and add your workplace's language
+                  requirements.
+                </p>
+                <form
+                  action="#"
+                  method="POST"
+                  className="gap-y-6 grid grid-cols-1"
+                >
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <label
+                        for="full-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Full name
+                      </label>
+                      <input
+                        type="text"
+                        name="full-name"
+                        id="full-name"
+                        autocomplete="name"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="Full name"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="email"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email
+                      </label>
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autocomplete="email"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="Email"
+                      />
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <label
+                        for="company-role"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Role in company
+                      </label>
+                      <input
+                        type="text"
+                        name="company-role"
+                        id="company-role"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="CEO, HR, etc..."
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="company-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Company name
+                      </label>
+                      <input
+                        type="text"
+                        name="company-name"
+                        id="company-name"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="Company name"
+                      />
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-2 gap-4">
+                    <div>
+                      <label
+                        for="phone"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Preferred language
+                      </label>
+                      <input
+                        type="text"
+                        name="language-1"
+                        id="language-1"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="Language 1"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="language-2"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Also considered
+                      </label>
+                      <input
+                        type="text"
+                        name="language-2"
+                        id="language-2"
+                        className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                        placeholder="Language 2"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      for="message"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Comments (max. 500 characters)
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows="4"
+                      className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm"
+                      placeholder="Message"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 inline-flex justify-center px-6 py-3 text-base font-medium text-white bg-blue-500 border border-transparent rounded-md shadow-sm"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
