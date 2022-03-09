7ca88152121ea77df4639903d01ceff80fa91b4b
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-09 11:20:42 +0100

    Remove unused componenet

--- src/pages/companies.en.jsx
+++ src/pages/companies.en.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link, graphql } from 'gatsby';
-import { GatsbyImage, getImage, getSrc } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
 
@@ -18,7 +18,7 @@ function employer(company) {
   let link = '/company/' + company.slug.current;
   let getImg = getImage(company.image.asset.gatsbyImageData);
   return (
-    <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+    <div className="bg-gray-50 flex justify-center col-span-1 px-8 py-8">
       <Link to={link}>
         <GatsbyImage
           image={getImg}
@@ -43,8 +43,8 @@ export default function Companies({ data }) {
       </div>
 
       <div className="bg-white">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-          <p className="text-left text-base font-semibold uppercase text-black tracking-wider">
+        <div className="max-w-7xl sm:px-6 lg:py-16 lg:px-8 px-4 py-12 mx-auto">
+          <p className="text-base font-semibold tracking-wider text-left text-black uppercase">
             Vetted euro employers
           </p>
           <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
