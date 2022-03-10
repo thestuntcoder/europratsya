3e6314510a8d8b0df95300ac69f055ce6a767e68
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-10 14:55:58 +0100

    Add job template

---
+++ src/templates/job.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { graphql } from 'gatsby';
+import LayoutPage from '../components/layout/layout-page';
+import NavCenter from '../components/layout/nav-center';
+import BlockContent from '../components/block-content';
+
+export const query = graphql`
+  query JobTemplateQuery($id: String!) {
+    job: sanityJobPost(id: { eq: $id }) {
+      description {
+        _rawEn
+      }
+      title {
+        en
+      }
+    }
+  }
+`;
+
+const JobPost = (props) => {
+  let job = props.data.job;
+
+  return (
+    <LayoutPage>
+      <Helmet>
+        <title>{job.title.en}</title>
+      </Helmet>
+
+      <div className="relative">
+        <NavCenter />
+      </div>
+
+      <div className="overflow-hidden bg-white">
+        <div className="max-w-7xl sm:px-6 lg:px-8 relative px-4 mx-auto mt-12">
+          <h1 className="mb-12 text-base text-3xl font-bold tracking-wide text-black">
+            {job.title.en}
+          </h1>
+        </div>
+      </div>
+
+      <div className="bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <BlockContent blocks={job.description._rawEn} />
+        </div>
+      </div>
+    </LayoutPage>
+  );
+};
+
+export default JobPost;
