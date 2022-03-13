e4688998c1fa050627a17beec0f9b91bd6339f40
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-13 13:25:39 +0100

    Add default SEO title and OG sharing image

--- src/components/layout/layout-page.jsx
+++ src/components/layout/layout-page.jsx
@@ -6,6 +6,13 @@ import Footer from './footer';
 export default function LayoutPage(content) {
   return (
     <div>
+      <Helmet>
+        <title>Europratsya</title>
+        <meta
+          property="og:image"
+          content="https://d10ngartke0vp.cloudfront.net/uk-field.jpg"
+        />
+      </Helmet>
       <Header />
       <div className="font-sans text-gray-900 antialiased">
         {content.children}
