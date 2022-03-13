23ea637a06d8e7f24243046bf49fb13e4ff4dda0
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-13 13:30:16 +0100

    Add more default header tags for social sharing on Twitter

--- src/components/layout/layout-page.jsx
+++ src/components/layout/layout-page.jsx
@@ -4,14 +4,22 @@ import Header from './header';
 import Footer from './footer';
 
 export default function LayoutPage(content) {
+  const metaTitle =
+    'Europratsya helps job-seekers who need to set up a temporary base for their families outside Ukraine.';
   return (
     <div>
       <Helmet>
-        <title>Europratsya</title>
+        <title>{metaTitle}</title>
+        <meta property="og:site_name" content={metaTitle} />
         <meta
           property="og:image"
           content="https://d10ngartke0vp.cloudfront.net/uk-field.jpg"
         />
+        <meta
+          name="twitter:image"
+          content="https://d10ngartke0vp.cloudfront.net/uk-field.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
       <Header />
       <div className="font-sans text-gray-900 antialiased">
