222972054c55d9f08da1715becd1fa069ade6361
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-13 13:34:31 +0100

    Add conventional file choosing form for CV upload

--- src/pages/contact-for-job-seekers.en.jsx
+++ src/pages/contact-for-job-seekers.en.jsx
@@ -156,44 +156,18 @@ export default function ContactEmployees() {
                     </div>
                   </div>
                   <div>
-                    <label className="block text-sm font-medium text-gray-700">
+                    <label
+                      for="file"
+                      lassName="block text-sm font-medium text-gray-700"
+                    >
                       Upload CV
                     </label>
-                    <div className="flex justify-center px-6 pt-5 pb-6 mt-1 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="w-12 h-12 mx-auto text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label
-                            for="file-upload"
-                            className="hover:text-blue-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500 relative font-medium text-blue-600 bg-white rounded-md cursor-pointer"
-                          >
-                            <span>Upload a CV file or drag and drop</span>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                              className="sr-only"
-                            />
-                          </label>
-                        </div>
-                        <p className="text-xs text-gray-500">
-                          PDF, JPG, DOC, DOCX up to 2MB
-                        </p>
-                      </div>
-                    </div>
+                    <input
+                      type="file"
+                      name="file"
+                      id="file"
+                      className="focus:ring-blue-500 focus:border-blue-500 block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm"
+                    />
                   </div>
                   <div>
                     <label
