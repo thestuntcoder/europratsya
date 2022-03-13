da7f424a77e84e5b85b30241406a39e536b8293f
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-14 00:29:00 +0100

    Add skills shortages and visa requirements to a country

--- src/templates/country.jsx
+++ src/templates/country.jsx
@@ -3,11 +3,30 @@ import { Helmet } from 'react-helmet';
 import { graphql } from 'gatsby';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
+import BlockContent from '../components/block-content';
 
 export const query = graphql`
   query CountryTemplateQuery($id: String!) {
     country: sanityCountry(id: { eq: $id }) {
       title
+    }
+
+    visa: sanityVisaRequirement(country: { _id: { eq: $id } }) {
+      description {
+        _rawEn
+      }
+      title {
+        en
+      }
+    }
+
+    skills: sanitySkillShortages(country: { _id: { eq: $id } }) {
+      description {
+        _rawEn
+      }
+      title {
+        en
+      }
     }
   }
 `;
@@ -29,6 +48,16 @@ const Country = (props) => {
       <div className="bg-white overflow-hidden">
         <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <h1>{title}</h1>
+
+          <h2 className="text-xl mt-8">{data.visa.title.en}</h2>
+          <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+            <BlockContent blocks={data.visa.description._rawEn} />
+          </div>
+
+          <h2 className="text-xl mt-8">{data.skills.title.en}</h2>
+          <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+            <BlockContent blocks={data.skills.description._rawEn} />
+          </div>
         </div>
       </div>
     </LayoutPage>
