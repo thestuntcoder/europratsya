b2291234f71efcd803c7462a3ed7ec6e73c939f9
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-15 11:17:52 +0100

    Add color to country subtitles

--- src/templates/country.jsx
+++ src/templates/country.jsx
@@ -42,7 +42,7 @@ function visa(visa) {
 
   return (
     <div>
-      <h2 className="text-xl mt-8">{visa.title.en}</h2>
+      <h2 className="text-xl mt-8 text-blue-500">{visa.title.en}</h2>
       <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <BlockContent blocks={visa.description._rawEn} />
       </div>
@@ -55,7 +55,7 @@ function skills(skills) {
 
   return (
     <div>
-      <h2 className="text-xl mt-8">{skills.title.en}</h2>
+      <h2 className="text-xl mt-8 text-blue-500">{skills.title.en}</h2>
       <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <BlockContent blocks={skills.description._rawEn} />
       </div>
