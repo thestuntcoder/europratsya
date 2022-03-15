4b9c0b91f043f2320c30616eb4eb6ba46bcd46b3
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-15 13:42:39 +0100

    Update svg attribute names

--- src/components/layout/nav-center.jsx
+++ src/components/layout/nav-center.jsx
@@ -77,9 +77,9 @@ export default class NavCenter extends React.Component {
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </div>
