d885d57e9d053d8480acf45f9731670dcee3b62a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-19 01:18:49 +0100

    Add skills in demand

---
+++ src/pages/skills-in-demand.uk.jsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet';
+import { Link, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import LayoutPage from '../components/layout/layout-page';
+import NavCenter from '../components/layout/nav-center.uk';
+
+export default function SkillsInDemandUk({ data }) {
+  const metaDescription = 'Посібники для отримання візи';
+  const metaTitle = 'Посібники для отримання візи';
+
+  data = data.allSanityCountry.nodes;
+  let countriesOutput = [];
+  for (var key in data) {
+    if (data[key].slug == null) continue;
+
+    countriesOutput.push(
+      <li key={key}>
+        <Link to={'/uk/countries/' + data[key].slug.current}>
+          {data[key].title.uk}
+        </Link>
+      </li>
+    );
+  }
+
+  return (
+    <LayoutPage lang="uk">
+      <Helmet>
+        <title>{metaTitle}</title>
+        <meta property="og:type" content="page" />
+        <meta property="og:title" content={metaTitle} />
+        <meta name="description" content={metaDescription} />
+        <meta property="og:description" content={metaDescription} />
+      </Helmet>
+
+      <div className="relative">
+        <NavCenter />
+      </div>
+
+      <div className="bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="mt-8">
+            <div className="mt-8 lg:mt-0">
+              <h1 className="text-base text-black font-bold text-3xl tracking-wide">
+                Посібники для отримання візи
+              </h1>
+              <div className="prose-indigo text-gray-500 text-lg mx-auto mt-8 lg:max-w-none lg:row-start-1 lg:col-start-1">
+                <div className="relative md:flex w-full">
+                  <div className="md:w-1/3">
+                    <ul>{countriesOutput}</ul>
+                  </div>
+                  <div className="md:w-2/3">
+                    <h2 className="text-base text-black font-bold text-xl tracking-wide">
+                      Карта безробіття
+                    </h2>
+                    <StaticImage
+                      className="my-8 h-full w-auto"
+                      src="../images/checklist/UKR-Unemployment-Map-2x.png"
+                      alt="Карта безробіття"
+                      loading="auto"
+                      objectFit="contain"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutPage>
+  );
+}
+
+export const COUNTRIES = graphql`
+  query allSanityCountryUk {
+    allSanityCountry(sort: { fields: title___uk, order: ASC }) {
+      nodes {
+        slug {
+          current
+        }
+        title {
+          uk
+        }
+        id
+      }
+    }
+  }
+`;
