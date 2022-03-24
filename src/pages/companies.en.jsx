bc7b3d96bad7b2fd0c04e0d8fde0002495e2221b
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-24 15:55:47 +0100

    Use Company helpers to increase code DRY (as much as currently possible)

--- src/pages/companies.en.jsx
+++ src/pages/companies.en.jsx
@@ -1,37 +1,9 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { Link, graphql } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { graphql } from 'gatsby';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
-
-function employersList(edges) {
-  let output = [];
-  for (var key in edges) {
-    output.push(employer(edges[key].node));
-  }
-
-  return output;
-}
-
-function employer(company) {
-  if (company.slug === null) return;
-
-  let link = '/company/' + company.slug.current;
-  let getImg = getImage(company.image.asset.gatsbyImageData);
-  return (
-    <div className="bg-gray-50 flex justify-center col-span-1 px-8 py-8">
-      <Link to={link}>
-        <GatsbyImage
-          image={getImg}
-          className="max-h-12"
-          alt={company.name}
-          objectFit="contain"
-        />
-      </Link>
-    </div>
-  );
-}
+import { employersList } from '../helpers/company';
 
 export default function Companies({ data }) {
   const metaDescription =
--- src/pages/companies.uk.jsx
+++ src/pages/companies.uk.jsx
@@ -1,37 +1,9 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { Link, graphql } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { graphql } from 'gatsby';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center.uk';
-
-function employersList(edges) {
-  let output = [];
-  for (var key in edges) {
-    output.push(employer(edges[key].node));
-  }
-
-  return output;
-}
-
-function employer(company) {
-  if (company.slug === null) return;
-
-  let link = '/uk/company/' + company.slug.current;
-  let getImg = getImage(company.image.asset.gatsbyImageData);
-  return (
-    <div className="bg-gray-50 flex justify-center col-span-1 px-8 py-8">
-      <Link to={link}>
-        <GatsbyImage
-          image={getImg}
-          className="max-h-12"
-          alt={company.name}
-          objectFit="contain"
-        />
-      </Link>
-    </div>
-  );
-}
+import { employersList } from '../helpers/company';
 
 export default function Companies({ data }) {
   const metaDescription =
@@ -59,7 +31,7 @@ export default function Companies({ data }) {
             Перевірені європейські роботодавці
           </p>
           <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-4 lg:mt-8">
-            {employersList(data.allSanityCompany.edges)}
+            {employersList(data.allSanityCompany.edges, 'uk')}
           </div>
         </div>
       </div>
