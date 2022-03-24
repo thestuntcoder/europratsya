9ccf605b3f7199f6d0d26202801f3a5b044e5ba8
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-24 13:52:36 +0100

    Simpify code in language helper to avoid switch/case

--- src/helpers/language.jsx
+++ src/helpers/language.jsx
@@ -1,39 +1,22 @@
 export function getRaw(obj, language = 'en') {
   if (obj == null) return '';
+  let altLanguage = language === 'en' ? '_rawUk' : '_rawEn';
 
-  switch (language) {
-    case 'en':
-      return typeof obj['_rawEn'] === 'undefined'
-        ? obj['_rawUk']
-        : obj['_rawEn'];
-      break;
+  language = '_raw' + language.charAt(0).toUpperCase() + language.slice(1);
 
-    case 'uk':
-      return typeof obj._rawUk === 'undefined' ? obj._rawEn : obj._rawUk;
-      break;
-
-    case 'de':
-      return typeof obj._rawDe === 'undefined' ? obj._rawEn : obj._rawDe;
-      break;
-  }
+  return typeof obj[language] === 'undefined'
+    ? obj[altLanguage]
+    : obj[language];
 }
 
 export function getTitle(obj, language = 'en') {
   if (obj == null) return '';
 
-  switch (language) {
-    case 'en':
-      return typeof obj.en === 'undefined' ? obj.uk : obj.en;
-      break;
+  let altLanguage = language === 'en' ? 'uk' : 'en';
 
-    case 'uk':
-      return typeof obj.uk === 'undefined' ? obj.en : obj.uk;
-      break;
-
-    case 'de':
-      return typeof obj.de === 'undefined' ? obj.en : obj.de;
-      break;
-  }
+  return typeof obj[language] === 'undefined'
+    ? obj[altLanguage]
+    : obj[language];
 }
 
 export function getSeo(obj, field, language, default_value = '') {
