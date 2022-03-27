a5ac8f00e4eff27c64ac9c6b0f8b21243a212a71
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-27 10:47:02 +0200

    Use helper functions on job ads component

--- src/components/job-ads.jsx
+++ src/components/job-ads.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import JobAd from './job-ad';
+import { getTitle, getUrlPrefix } from '../helpers/language';
 
 export default function JobAds(props) {
   let output = [];
@@ -16,12 +17,7 @@ export default function JobAds(props) {
     )
       continue;
 
-    let desc =
-      lang === 'uk'
-        ? props.data[key].node.description.uk == null
-          ? props.data[key].node.description.en
-          : props.data[key].node.description.uk
-        : props.data[key].node.description.en;
+    let desc = getTitle(props.data[key].node.description, lang);
 
     output.push(
       <JobAd
@@ -29,24 +25,16 @@ export default function JobAds(props) {
         location={
           props.data[key].node.city +
           ', ' +
-          (lang === 'uk'
-            ? props.data[key].node.country.title.uk
-            : props.data[key].node.country.title.en)
+          getTitle(props.data[key].node.country.title, lang)
         }
         profession={props.data[key].node.job_categories[0].title}
         expiraition_date={props.data[key].node.validUntil}
         salary={props.data[key].node.salary}
-        title={
-          lang === 'uk'
-            ? props.data[key].node.title.uk
-            : props.data[key].node.title.en
-        }
+        title={getTitle(props.data[key].node.title, lang)}
         contact_detail={props.data[key].node.contact}
         description={desc[0].children[0].text.substring(0, 200) + '...'}
         linkto={
-          (lang === 'uk' ? '/uk' : '') +
-          '/job/' +
-          props.data[key].node.slug.current
+          getUrlPrefix(lang) + '/job/' + props.data[key].node.slug.current
         }
         key={key}
       />
