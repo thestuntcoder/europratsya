ffd01628a16560294c8c2d456aa195f6e2182c9a
Jovana Ranđelović <jovana@stuntcoders.com>
2022-03-28 10:15:11 +0200

    Update search-result component to add custom highlight on search
    Minor style updates for search results

--- src/components/search/search-result.jsx
+++ src/components/search/search-result.jsx
@@ -2,7 +2,7 @@ import { default as React } from 'react';
 import {
   connectStateResults,
   connectHits,
-  Highlight,
+  connectHighlight,
   Index,
 } from 'react-instantsearch-dom';
 
@@ -10,29 +10,45 @@ const HitCount = connectStateResults(({ searchResults }) => {
   const hitCount = searchResults && searchResults.nbHits;
 
   return hitCount > 0 ? (
-    <div className="text-xs text-right mx-4">
+    <div className="text-xs text-right mx-4 italic">
       {hitCount} result{hitCount !== 1 ? `s` : ``}
     </div>
   ) : (
-    <div className="text-xs text-right mx-4">No results</div>
+    <div className="text-xs text-right mx-4 italic">No results</div>
   );
 });
 
-const PageHit = ({ hit }) => (
-  <div className="flex">
-    <a href={hit.path}>
-      <h4 className="text-sm mx-4">
-        <Highlight attribute="path" hit={hit} tagName="mark" />
-      </h4>
-    </a>
-  </div>
-);
+const Highlight = ({ highlight, attribute, hit }) => {
+  const parsedHit = highlight({
+    highlightProperty: '_highlightResult',
+    attribute,
+    hit,
+  });
+
+  return (
+    <span>
+      {parsedHit.map((part, index) =>
+        part.isHighlighted ? (
+          <mark className="bg-blue-200" key={index}>
+            {part.value}
+          </mark>
+        ) : (
+          <span key={index}>{part.value}</span>
+        )
+      )}
+    </span>
+  );
+};
+
+const CustomHighlight = connectHighlight(Highlight);
 
 const Hits = ({ hits }) => (
-  <ol className="flex flex-col space-y-2 text-sm">
+  <ol className="flex flex-col text-sm">
     {hits.map((hit) => (
-      <li className="px-2" key={hit.path}>
-        <a href={hit.path}>{hit.path}</a>
+      <li className="px-2 py-1 hover:bg-gray-50" key={hit.path}>
+        <a className="block" href={hit.path}>
+          <CustomHighlight attribute="path" hit={hit} tagName="mark" />
+        </a>
       </li>
     ))}
   </ol>
@@ -43,7 +59,7 @@ const CustomHits = connectHits(Hits);
 const HitsInIndex = ({ index }) => (
   <Index indexName={index.name}>
     <HitCount />
-    <CustomHits className="Hits" hitComponent={PageHit} />
+    <CustomHits />
   </Index>
 );
 
