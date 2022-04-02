117518c3162e3da31425d899c0156b3c6a647a38
Dejan Jacimovic <dejan@stuntcoders.com>
2022-04-02 23:55:05 +0200

    Move code from helpers to components for filtering languages and countries

---
+++ src/components/filter-country.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+export default function FilterCountry(props) {
+  let list = [];
+  let duplicates = [];
+  let edges = props.edges;
+
+  for (var key in edges) {
+    if (edges[key].node.employer == null) continue;
+    if (edges[key].node.slug == null) continue;
+
+    const title = getTitle(edges[key].node.country.title, lang);
+    if (duplicates.includes(title)) continue;
+
+    duplicates.push(title);
+    list.push(
+      <option value={title} key={'country-' + key}>
+        {title}
+      </option>
+    );
+  }
+
+  return (
+    <select
+      name="country"
+      className="form-select appearance-none block w-1/2 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-500 focus:outline-none float-right"
+    >
+      {list}
+    </select>
+  );
+}
---
+++ src/components/filter-language.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+export default function FilterLanguage(props) {
+  let list = [];
+  let duplicates = [];
+  let edges = props.edges;
+
+  for (var key in edges) {
+    if (edges[key].node.employer == null) continue;
+    if (edges[key].node.slug == null) continue;
+
+    for (let lKey in edges[key].node.job_languages) {
+      const title = edges[key].node.job_languages[lKey].name;
+      if (duplicates.includes(title)) continue;
+
+      duplicates.push(title);
+      list.push(
+        <option value={title} key={'job-languages-' + key}>
+          {title}
+        </option>
+      );
+    }
+  }
+
+  return (
+    <select
+      name="job_languages"
+      className="form-select appearance-none block w-1/2 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-500 focus:outline-none float-right"
+    >
+      {list}
+    </select>
+  );
+}
