5dd2731d178823f51e7201dea24befc0d012e979
Dejan Jacimovic <dejan@stuntcoders.com>
2022-04-03 00:46:02 +0200

    Propagate event from parent to child

--- src/components/filter-country.jsx
+++ src/components/filter-country.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import { getTitle } from '../helpers/language';
 
 export default function FilterCountry(props) {
@@ -26,6 +25,7 @@ export default function FilterCountry(props) {
     <select
       name="country"
       className="form-select appearance-none block w-1/2 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-500 focus:outline-none float-right"
+      onChange={(event) => props.changeCountry(event)}
     >
       {list}
     </select>
