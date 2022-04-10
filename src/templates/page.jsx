c01941ab5566965aed398b75304b0a7b1df4d699
Dejan Jacimovic <dejan@stuntcoders.com>
2022-04-10 13:24:19 +0200

    Fix all the issues of page generation

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -1,34 +1,95 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import LayoutPage from '../components/layout/layout-page';
 import NavCenter from '../components/layout/nav-center';
 import NavCenterUk from '../components/layout/nav-center.uk';
 import NavCenterDe from '../components/layout/nav-center.de';
-import JobAds from '../components/job-ads';
 import BlockContent from '../components/block-content';
-import { getRaw, getUrlPrefix } from '../helpers/language';
+import { getRaw, getTitle } from '../helpers/language';
+
+function getSidebar(getImg, description) {
+  return (
+    <div className="relative lg:col-start-2 lg:row-start-1">
+      <svg
+        className="absolute top-0 right-0 -mt-20 -mr-20 hidden lg:block"
+        width="404"
+        height="384"
+        fill="none"
+        viewBox="0 0 404 384"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+            x="0"
+            y="0"
+            width="20"
+            height="20"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect
+              x="0"
+              y="0"
+              width="4"
+              height="4"
+              className="text-gray-200"
+              fill="currentColor"
+            />
+          </pattern>
+        </defs>
+        <rect
+          width="404"
+          height="384"
+          fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
+        />
+      </svg>
+      <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
+        <figure>
+          <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+            <GatsbyImage
+              image={getImg}
+              className="rounded-lg object-cover object-center shadow-lg"
+              alt="sth"
+              objectFit="contain"
+            />
+          </div>
+          <figcaption className="mt-3 flex text-sm italic text-gray-500">
+            <svg
+              className="h-5 w-5 flex-none text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="ml-2">{description}</span>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+}
 
 export default function Page(props) {
   const language = props.pageContext.language;
+
   let page = props.data.page;
+  let sidebar = page.sidebarImage === null;
 
-  let getImg = getImage(page.sidebarImage.asset.gatsbyImageData);
+  let metaTitle = getTitle(page.title, language);
+  let metaDescription = getTitle(page.body, language);
 
-  let metaTitle = page.title;
-  let metaDescription = page.title;
-  let metaImage = page.sidebarImage.asset.gatsbyImageData.images.fallback.src;
-
-  if (page.seo != null && page.seo.title_en != null) {
-    metaTitle = page.seo.title_en;
+  if (sidebar) {
+    // let metaImage = page.sidebarImage.asset.gatsbyImageData.images.fallback.src;
   }
-  if (page.seo != null && page.seo.description_en != null) {
-    metaDescription = page.seo.description_en;
-  }
-  if (page.seo != null && page.seo.seo_image != null) {
-    metaImage = page.seo.seo_image.asset.gatsbyImageData.images.fallback.src;
-  }
+  console.log(page.sidebarImage, page.sidebarImage != null);
 
   const descriptionRaw = getRaw(page.body, language);
   let navigation = {};
@@ -60,82 +121,39 @@ export default function Page(props) {
 
       <div className="overflow-hidden bg-white">
         <div className="relative mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-          <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
-          <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
+          {page.sidebarImage != null ? (
+            <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
+          ) : (
+            ''
+          )}
+          <div
+            className={
+              'mx-auto max-w-prose text-base lg:max-w-none ' +
+                page.sidebarImage !=
+              null
+                ? 'lg:grid lg:grid-cols-2 lg:gap-8'
+                : ''
+            }
+          >
             <div>
               <h1 className="text-base text-3xl font-bold tracking-wide text-black">
-                About us
+                {getTitle(page.title, language)}
               </h1>
             </div>
           </div>
-          <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
-            <div className="relative lg:col-start-2 lg:row-start-1">
-              <svg
-                className="absolute top-0 right-0 -mt-20 -mr-20 hidden lg:block"
-                width="404"
-                height="384"
-                fill="none"
-                viewBox="0 0 404 384"
-                aria-hidden="true"
-              >
-                <defs>
-                  <pattern
-                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
-                    x="0"
-                    y="0"
-                    width="20"
-                    height="20"
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect
-                      x="0"
-                      y="0"
-                      width="4"
-                      height="4"
-                      className="text-gray-200"
-                      fill="currentColor"
-                    />
-                  </pattern>
-                </defs>
-                <rect
-                  width="404"
-                  height="384"
-                  fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
-                />
-              </svg>
-              <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
-                <figure>
-                  <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
-                    <GatsbyImage
-                      image={getImg}
-                      className="rounded-lg object-cover object-center shadow-lg"
-                      alt="sth"
-                      objectFit="contain"
-                    />
-                  </div>
-                  <figcaption className="mt-3 flex text-sm italic text-gray-500">
-                    <svg
-                      className="h-5 w-5 flex-none text-gray-400"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    <span className="ml-2">
-                      Vernadsky polar research station. 12 Ukrainian scientists
-                      winter here for 10 months in extreme conditions,
-                      delivering widely acclaimed climate change research.{' '}
-                    </span>
-                  </figcaption>
-                </figure>
-              </div>
-            </div>
+          <div
+            className={
+              'mt-8 ' + page.sidebarImage != null
+                ? 'lg:grid lg:grid-cols-2 lg:gap-8'
+                : ''
+            }
+          >
+            {page.sidebarImage != null
+              ? getSidebar(
+                  getImage(page.sidebarImage.asset.gatsbyImageData),
+                  getTitle(page.footnote, language)
+                )
+              : ''}
             <div className="mt-8 lg:mt-0">
               <div className="prose prose-indigo mx-auto text-lg text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none">
                 <BlockContent blocks={descriptionRaw} />
