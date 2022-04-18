7ef9edaf3f016e7669b7cb98fff065620c8892f8
Dejan Jacimovic <dejan@stuntcoders.com>
2022-04-18 15:15:49 +0200

    Add new, unified index page for all languages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,14 +17,18 @@ import sponsorGrieg from '../images/sponsors/grieg.svg';
 import sponsorTM from '../images/sponsors/tm.png';
 
 export default function Home({ data }) {
-  const metaDescription =
-    'Our web portal helps job-seekers who need to set up a temporary base for their families outside Ukraine. We also post remote jobs suitable for workers based inside Ukraine. One Ukrainian-language database for skills in demand and vacancies across Europe - free for candidates and free for employers.';
-  const metaTitle = 'Non-profit job portal for Ukrainian job-seekers in Europe';
+  const { t, i18n } = useTranslation();
+  const language = i18n.language;
 
-  const { t } = useTranslation();
+  const metaDescription = t(
+    'Our web portal helps job-seekers who need to set up a temporary base for their families outside Ukraine. We also post remote jobs suitable for workers based inside Ukraine. One Ukrainian-language database for skills in demand and vacancies across Europe - free for candidates and free for employers.'
+  );
+  const metaTitle = t(
+    'Non-profit job portal for Ukrainian job-seekers in Europe'
+  );
 
   return (
-    <LayoutPage>
+    <LayoutPage lang={language}>
       <Helmet>
         <title>{metaTitle}</title>
         <meta name="description" content={metaDescription} />
@@ -41,12 +45,14 @@ export default function Home({ data }) {
             <img
               className="h-full w-full object-cover"
               src={ukrBg}
-              alt="We welcome Ukrainians to European Union."
+              alt={t('We welcome Ukrainians to European Union.')}
             />
           </div>
           <div className="relative mx-auto max-w-7xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
             <h1 className="text-center text-3xl font-extrabold tracking-tight text-white drop-shadow-md">
-              {t('heading')}
+              {t(
+                'Our portal assists European employers in finding skilled Ukrainian workers.'
+              )}
             </h1>
           </div>
         </div>
@@ -59,14 +65,19 @@ export default function Home({ data }) {
             <div className="flex flex-col rounded-2xl bg-gray-800 shadow-xl">
               <div className="relative flex-1 px-6 pt-12 pb-8 text-center md:px-8">
                 <h3 className="text-2xl font-extrabold text-white">
-                  <Trans>c1</Trans>
+                  <Trans>Candidates</Trans>
                 </h3>
                 <p className="mt-4 text-base text-white">
-                  Please check our work visa guides and skills shortages guide{' '}
-                  <Link to="/skills-in-demand/">country by country</Link>.
+                  <Trans>
+                    Please check our work visa guides and skills shortages guide
+                  </Trans>
+                  <Link to="/skills-in-demand/">
+                    <Trans>country by country</Trans>
+                  </Link>
+                  .
                 </p>
                 <p className="mt-8 text-base text-white">
-                  Do you need CV translation help?
+                  <Trans>Do you need CV translation help?</Trans>
                 </p>
                 <div className="mt-8">
                   <div className="inline-flex rounded-full shadow">
@@ -74,7 +85,7 @@ export default function Home({ data }) {
                       to="/contact-for-job-seekers/"
                       className="inline-flex items-center justify-center rounded-full border border-transparent bg-white px-5 py-3 text-base font-medium uppercase text-black hover:bg-gray-200"
                     >
-                      Contact us
+                      <Trans>Contact us</Trans>
                     </Link>
                   </div>
                 </div>
@@ -84,21 +95,22 @@ export default function Home({ data }) {
             <div className="flex flex-col rounded-2xl bg-blue-500 shadow-xl">
               <div className="relative flex-1 px-6 pt-12 pb-8 text-center md:px-8">
                 <h3 className="text-2xl font-extrabold text-white">
-                  Employers
+                  <Trans>Employers</Trans>
                 </h3>
                 <p className="mt-4 text-base text-white">
-                  To evaluate Ukrainian CVs, please check our{' '}
+                  <Trans>To evaluate Ukrainian CVs, please check our</Trans>
+                  {` `}
                   <Link to="/ukr-eduction-explainer/">
-                    UKR education explainer
+                    <Trans>UKR education explainer</Trans>
                   </Link>{' '}
-                  and our{' '}
+                  <Trans>and our</Trans>{' '}
                   <Link to="/ukr-expertise-explained/">
-                    UKR expertise explainer
+                    <Trans>UKR expertise explainer</Trans>
                   </Link>
                   .
                 </p>
                 <p className="mt-8 text-base text-white">
-                  Register to post your vacancies
+                  <Trans>Register to post your vacancies</Trans>
                 </p>
                 <div className="mt-8">
                   <div className="inline-flex rounded-full shadow">
@@ -106,7 +118,7 @@ export default function Home({ data }) {
                       to="/contact-for-employers/"
                       className="inline-flex items-center justify-center rounded-full border border-transparent bg-white px-5 py-3 text-base font-medium uppercase text-black hover:bg-gray-200"
                     >
-                      Join
+                      <Trans>Join</Trans>
                     </Link>
                   </div>
                 </div>
@@ -120,13 +132,17 @@ export default function Home({ data }) {
         <div className="relative px-4 sm:px-6 lg:px-8">
           <div className="prose prose-indigo prose-lg mx-auto mt-6 text-gray-500">
             <p>
-              Europratsya post jobs for candidates who need to set up a
-              temporary base for their families outside Ukraine. We also post
-              remote jobs suitable for workers based inside Ukraine.
+              <Trans>
+                Europratsya post jobs for candidates who need to set up a
+                temporary base for their families outside Ukraine. We also post
+                remote jobs suitable for workers based inside Ukraine.
+              </Trans>
             </p>
             <p>
-              One Ukrainian-language database for skills in demand and vacancies
-              across Europe - free for candidates and employers.
+              <Trans>
+                One Ukrainian-language database for skills in demand and
+                vacancies across Europe - free for candidates and employers.
+              </Trans>
             </p>
           </div>
         </div>
@@ -139,10 +155,14 @@ export default function Home({ data }) {
         <div className="relative mx-auto max-w-7xl">
           <div className="text-center">
             <h2 className="text-3xl font-extrabold tracking-tight text-yellow-400 sm:text-4xl">
-              Latest vacancies
+              <Trans>Latest vacancies</Trans>
             </h2>
           </div>
-          <JobAds limit="9" lang="en" data={data.allSanityJobPost.edges} />
+          <JobAds
+            limit="9"
+            lang={language}
+            data={data.allSanityJobPost.edges}
+          />
 
           <div className="mt-8 text-center">
             <div className="inline-flex rounded-full shadow">
@@ -150,7 +170,7 @@ export default function Home({ data }) {
                 to="/vacancies"
                 className="inline-flex items-center justify-center rounded-full border border-transparent bg-blue-500 px-5 py-3 text-base font-medium text-white hover:bg-blue-700"
               >
-                See all vacancies
+                <Trans>See all vacancies</Trans>
               </Link>
             </div>
           </div>
@@ -168,10 +188,12 @@ export default function Home({ data }) {
         <div className="absolute inset-0 bg-gray-900 opacity-30" />
         <div className="z-10 mx-auto flex max-w-7xl items-center justify-between">
           <div className="z-10 m-auto mx-auto mt-64 mb-64 text-center align-middle text-4xl font-extrabold text-white">
-            Several Nobel prize winners in medicine, physics, chemistry and
-            literature hail from Ukraine. Its oldest universities are from the
-            16th and 17th century, and continue to produce world-class graduates
-            in sciences, engineering, agriculture and mining.
+            <Trans>
+              Several Nobel prize winners in medicine, physics, chemistry and
+              literature hail from Ukraine. Its oldest universities are from the
+              16th and 17th century, and continue to produce world-class
+              graduates in sciences, engineering, agriculture and mining.
+            </Trans>
           </div>
         </div>
       </div>
@@ -179,7 +201,7 @@ export default function Home({ data }) {
       <div className="bg-white">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
           <p className="text-center text-base font-semibold uppercase tracking-wider text-gray-600">
-            Sponsors and partners
+            <Trans>Sponsors and partners</Trans>
           </p>
           <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
             <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
@@ -212,7 +234,22 @@ export default function Home({ data }) {
 }
 
 export const JOB_POSTS = graphql`
-  query JobPostsIndex {
+  query JobPostsIndex($language: String!) {
+    locales: allLocale(
+      filter: {
+        ns: { in: ["translation", "index"] }
+        language: { eq: $language }
+      }
+    ) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+
     allSanityJobPost(sort: { fields: _createdAt, order: DESC }) {
       edges {
         node {
