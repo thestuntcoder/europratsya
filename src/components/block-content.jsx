3e5aab420f2f6fbcb47c1a4c8f3360d9549f784a
Dejan Jacimovic <dejan@stuntcoders.com>
2022-05-06 22:34:52 +0200

    Rewrite block content to migrate everything except links

--- src/components/block-content.jsx
+++ src/components/block-content.jsx
@@ -1,70 +1,58 @@
 import React from 'react';
-import BaseBlockContent from '@sanity/block-content-to-react';
+// import BaseBlockContent from '@sanity/block-content-to-react';
+import { PortableText } from '@portabletext/react';
 
 const serializers = {
   types: {
-    image: ({ value }) => '',
-    block(props) {
-      switch (props.node.style) {
-        case 'h1':
-          return (
-            <h1 className="mt-8 text-base text-3xl font-bold tracking-wide text-black">
-              {props.children}
-            </h1>
-          );
-
-        case 'h2':
-          return (
-            <h2 className="mt-4 text-base text-2xl font-bold tracking-wide text-black">
-              {props.children}
-            </h2>
-          );
-
-        case 'h3':
-          return (
-            <h3 className="mt-2 text-xl font-bold tracking-wide text-black">
-              {props.children}
-            </h3>
-          );
-
-        case 'h4':
-          return (
-            <h4 className="mt-2 font-bold tracking-wide text-black">
-              {props.children}
-            </h4>
-          );
-
-        case 'blockquote':
-          return <blockquote>{props.children}</blockquote>;
-
-        default:
-          return <p className="my-4">{props.children}</p>;
-      }
-    },
-    code(props) {
-      return (
-        <pre data-language={props.node.language}>
-          <code>{props.node.code}</code>
-        </pre>
-      );
+    image: ({ value }) => {
+      console.log(value);
+      return null;
     },
   },
-  list: (props) =>
-    props.type === 'bullet' ? (
-      <ul>{props.children}</ul>
-    ) : (
-      <ol>{props.children}</ol>
+  block: {
+    h1: ({ children }) => (
+      <h1 className="mt-8 text-base text-3xl font-bold tracking-wide text-black">
+        {children}
+      </h1>
     ),
-  listItem: (props) => <li className="ml-4 list-disc">{props.children}</li>,
+    h2: ({ children }) => (
+      <h2 className="mt-4 text-base text-2xl font-bold tracking-wide text-black">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="mt-2 text-xl font-bold tracking-wide text-black">
+        {children}
+      </h3>
+    ),
+    h4: ({ children }) => (
+      <h4 className="mt-2 font-bold tracking-wide text-black">{children}</h4>
+    ),
+    blockquote: ({ children }) => <blockquote>{children}</blockquote>,
+    code: ({ children }) => (
+      <pre data-language={children.language}>
+        <code>{children.code}</code>
+      </pre>
+    ),
+    p: ({ children }) => <p className="my-4">{children}</p>,
+  },
+  list: {
+    bullet: ({ children }) => <ul>{children}</ul>,
+    number: ({ children }) => <ol>{children}</ol>,
+  },
+  listItem: {
+    bullet: ({ children }) => <li className="ml-4 list-disc">{children}</li>,
+    checkmarks: ({ children }) => <li>{children}</li>,
+  },
   marks: {
-    strong: (props) => <strong>{props.children}</strong>,
-    em: (props) => <em>{props.children}</em>,
-    link: (props) => {
+    strong: ({ children }) => <strong>{children}</strong>,
+    em: ({ children }) => <em>{children}</em>,
+    link: ({ children }) => {
       let rel,
         blank = '';
       if (
-        typeof props.mark.href !== 'undefined' &&
-        !props.mark.href.startsWith('/')
+        typeof children.mark.href !== 'undefined' &&
+        !children.mark.href.startsWith('/')
       ) {
         rel = 'noreferrer noopener';
         blank = '_blank';
@@ -72,20 +60,20 @@ const serializers = {
 
       return (
         <a
-          href={props.mark.href}
+          href={children.mark.href}
           rel={rel}
           target={blank}
           className="text-blue-500 hover:underline"
         >
-          {props.children}
+          {children}
         </a>
       );
     },
   },
 };
 
-const BlockContent = ({ blocks }) => (
-  <BaseBlockContent blocks={blocks} serializers={serializers} />
-);
+const BlockContent = (props) => {
+  return <PortableText value={props.value} components={serializers} />;
+};
 
 export default BlockContent;
